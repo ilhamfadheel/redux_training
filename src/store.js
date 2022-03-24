9aaec804cee6e0c80cb7752c9dd42cbee6623249
Ilham Fadhil <ilham.sykes@hotmail.com>
2022-03-24 14:06:52 +0700

    move file

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import { devToolsEnhancer } from "@redux-devtools/extension";
 
