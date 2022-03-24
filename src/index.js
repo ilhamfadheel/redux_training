aa766e86c79fcf42c5be181ed5c9b1928eb82b0c
Ilham Fadhil <ilham.sykes@hotmail.com>
2022-03-24 14:29:46 +0700

    change prop

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ store.subscribe(() => {
   console.log("Store changed!", store.getState());
 });
 
-store.dispatch(userAdded({ name: "slamet", balance: 999999 }));
-store.dispatch(userAdded({ name: "yanto", balance: -999999 }));
+store.dispatch(userAdded({ name: "Ilham", balance: 100 }));
+store.dispatch(userAdded({ name: "Fadhil", balance: 200 }));
 
-store.dispatch(productAdded({ name: "Product 1", price: 500 }));
-store.dispatch(productAdded({ name: "Product 2", price: 1000 }));
+store.dispatch(productAdded({ name: "Sabun", price: 10 }));
+store.dispatch(productAdded({ name: "Shampoo", price: 20 }));
 store.dispatch(productMarkAsDiscount({ id: 1, hasDiscount: true }));
 
 store.dispatch(productCartAdded({ productId: 1, quantity: 2 }));
