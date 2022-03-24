c67d2713fe4a5a79a79c117dd920ad77970c993d
Ilham Fadhil <ilham.sykes@hotmail.com>
2022-03-24 14:15:45 +0700

    remove comment

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,3 @@
-// import { devToolsEnhancer } from '@redux-devtools/extension';
-// import { createStore } from 'redux';
-// import reducer from './product/reducer'; // folder approach
-// const store = createStore(reducer, devToolsEnhancer({ trace: true }));
-// import reducer from './product';
 import reducer from "./root-reducer";
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "./middleware/logger";
